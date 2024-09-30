b67ffaa74c8a2e3549bb8c42ed72ab93bb60daa1
mmaalin <maalin.622139@swl.k12.oh.us>
2024-09-30 08:55:48 -0400

    need 2 finish 10/1

--- src/CtoFConverterin.java
+++ src/CtoFConverterin.java
@@ -1,5 +1,26 @@
-public class Main {
+import java.util.Scanner;
+
+public class CtoFConverterin {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double temp;
+        double tempInF;
+        boolean validTemp = false;
+
+        Scanner scan = new Scanner (System.in);
+
+        do {
+            System.out.println("Please input a temperature to convert from Celsius to Fahrenheit:");
+            if (scan.hasNextDouble()) {
+                validTemp = true;
+                temp = scan.nextDouble();
+                tempInF =  (temp*9/5) + 32;
+                System.out.printf("%-10s%.2f", "Degrees C ", " " + temp);
+                System.out.printf("\n%-10fs.2f", "Degrees F ", " " + tempInF); // fix these two printf's tmrw 10/1
+
+            }
+            scan.nextLine();
+        } while (!validTemp);
+
     }
 }
